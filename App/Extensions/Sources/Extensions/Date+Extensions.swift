3e58e23c5c150a25390b021c9b350f8dff6a854c
ksh <ksh@skywinds.tech>
2020-10-11 09:53:20 +0530

    Date - remaining time extension

--- App/Extensions/Sources/Extensions/Date+Extensions.swift
+++ App/Extensions/Sources/Extensions/Date+Extensions.swift
@@ -26,4 +26,33 @@ public extension Date {
         return calendar.date(byAdding: .day, value: 1, to: self.withoutTime)!
     }
     
+    func addPrefix(time: String) -> String {
+        var timeWithPrefix = time
+        if timeWithPrefix.lengthOfBytes(using: .utf8) == 1 {
+            timeWithPrefix = "0" + time
+        }
+        return timeWithPrefix
+    }
+    
+    func remainingTimeString(toDate: Date) -> String {
+        let calendar = Calendar.current
+        let diffDateComponents = calendar.dateComponents([.hour, .minute, .second], from: self, to: toDate)
+        var countdownSTR = ""
+        
+        guard let hours = diffDateComponents.hour,
+              let minutes = diffDateComponents.minute,
+              let seconds = diffDateComponents.second else {
+            return "00:00"
+        }
+        if hours >= 0 || minutes >= 0 || seconds >= 0 {
+            let hourSTR = "\(String(describing: hours))"
+            countdownSTR = addPrefix(time: hourSTR) + ":"
+            let minSTR = "\(String(describing: minutes))"
+            countdownSTR += addPrefix(time: minSTR) + ":"
+            let secondSTR = "\(String(describing: seconds))"
+            countdownSTR += addPrefix(time: secondSTR)
+            return countdownSTR
+        }
+        return "00:00"
+    }
 }
